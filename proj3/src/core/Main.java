c6879ad291ab44442ce041761481d6224a3e149f
nnnnnorway <s63640365487@gmail.com>
2024-08-21 14:08:03 +0800

    start

---
+++ proj3/src/core/Main.java
@@ -0,0 +1,11 @@
+package core;
+
+public class Main {
+    public static void main(String[] args) {
+        World world = new World(60, 60);
+        // build your own world!
+
+
+
+    }
+}
---
+++ proj3/src/core/World.java
@@ -0,0 +1,20 @@
+package core;
+import java.util.Scanner;
+public class World {
+
+    // build your own world!
+    public static final int Width = 60;
+    public static final int Height = 50;
+
+    public World(long seed){
+    }
+
+    public long getSeed(String input){
+        return Long.parseLong(input.substring(1, input.length() - 1));
+    }
+
+    public String getInput(){
+        String str = s;
+        return
+    }
+}
